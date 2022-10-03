0800d76d8f4c0a7d9bc264ab7d5c1e7961003590
captjay98WSL <captjay98@gmail.com>
2022-10-03 14:37:16 +0100

    Done

---
+++ firstJS.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/*
+function nextInLine(arr, item){
+    arr.push(item);
+    return arr.shift();
+}
+
+var  test = [5, 6, 7, 8, 9];
+
+console.log(test);
+console.log(nextInLine(test, 55));
+//console.log(test);
+
+let val = 9;
+
+if  (val !== '9') {
+    console.log(val);
+}*/
+
+basically i am learning javascript
+i am still onit 
+holding on
+still codig
+getting tired now omo  din tire o
+not there yet
+done
+expprt function double 
---
+++ ninja.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Ninja{
+    constructor(name, rank, power){ 
+        this.name = name;
+        this.rank = rank;
+        this.power = power;
+    }
+
+    iam(){
+        console.log("i am " + this.name + " i am the " + this.rank);
+    }
+}
+
+class Leaf extends Ninja {
+
+    iam(){
+        super.iam()
+        console.log("from the Leaf Village")
+    }
+}
+
+class Sand extends Ninja {
+
+    iam(){
+        super.iam()
+        console.log("from the Sand Village")
+    }
+}
+
+naruto = new Leaf("Naruto", "Hokage", 999);
+naruto.iam();
+
+gaara = new Sand("gaara", "Hokage", 950);
+gaara.iam();
